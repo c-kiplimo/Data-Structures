b44e99812a0e6d29f94f737b4612da91d7bc025f
kiplimo-dev <kiplimocollins855@gmail.com>
2023-01-05 22:43:48 +0300

    single dimensional array practice

--- Arrays/src/com/dataStructures/SingleDimensionalArray.java
+++ Arrays/src/com/dataStructures/SingleDimensionalArray.java
@@ -3,17 +3,21 @@ import java.util.Arrays;
 
 public class SingleDimensionalArray {
 
-   int arr[]=null;
-   public SingleDimensionalArray(int sizeofArray){
-       arr=new int[sizeofArray];
-       for(int i=0;i<arr.length;i++){
-           arr[i]=Integer.MIN_VALUE;
-       }
-   }
-   public void insert(int location,int valueToBeInserted){
- tryarr[location]==Integer.MIN_VALUE){
-arr[location]=valueToBeInserted;
-       System.out.println("Successfully Inserted");
-}else{
-       System.out.println("This cell is already occupied");
-}}
+    int arr[] = null;
+
+    public SingleDimensionalArray(int sizeofArray) {
+        arr = new int[sizeofArray];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public void insert(int location, int valueToBeInserted) {
+        if (arr[location] == Integer.MIN_VALUE) {
+            arr[location] = valueToBeInserted;
+            System.out.println("Successfully Inserted");
+        } else {
+            System.out.println("This cell is already occupied");
+        }
+    }
+}
