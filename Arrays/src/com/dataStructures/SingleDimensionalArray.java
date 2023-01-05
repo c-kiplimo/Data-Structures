798b1b742738fd80d69b7636d9713cfb54368de7
kiplimo-dev <kiplimocollins855@gmail.com>
2023-01-05 22:46:20 +0300

    fixed an issue in the array

--- Arrays/src/com/dataStructures/SingleDimensionalArray.java
+++ Arrays/src/com/dataStructures/SingleDimensionalArray.java
@@ -2,11 +2,13 @@ package com.dataStructures;
 import java.util.Arrays;
 
 public class SingleDimensionalArray {
-
+//initialize array
     int arr[] = null;
 
     public SingleDimensionalArray(int sizeofArray) {
+        //instatiantiate the array
         arr = new int[sizeofArray];
+        //initialize with the minimum integer detail
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.MIN_VALUE;
         }
